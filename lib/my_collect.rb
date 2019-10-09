ca71df01d5838d477431d98dd3e5aedb25d27658
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2019-10-09 17:09:44 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,10 @@
 def collect(collection)
   i=1
-
+  new_collection=[]
   while i<collection.length
-    yield collection
+    new_collection<<
+    yield collection[1]
+    i+=1
+  end
+  new_collection
 end
